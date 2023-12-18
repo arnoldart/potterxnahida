bb30f57d901716a5814ef2c36e9e79cc079f2078
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2023-12-18 20:54:33 +0700

    feat: haiyaaa

---
+++ frontend/app/(user)/(subscribe)/roadmapBlue/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+const RoadMapBluePage = () => {
+  return (
+    <div className=''>
+      <Image
+        src="/image/roadmap_blue.png"
+        alt="roadmap_blue"
+        width={1000}
+        height={1000}
+      />
+    </div>
+  )
+}
+
+export default RoadMapBluePage
